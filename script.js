bf056c255fd13f08029ff9e54c7d7f06692bc0dc
hjrim94 <62858164+hjrim94@users.noreply.github.com>
2024-01-18 21:28:55 -0500

    Create function that runs a  single round of rock, paper, scissors game based on user input

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 function getComputerChoice () {
     if (Math.random()<= (1/3)) {
-        return 'Rock'
+        return 'rock'
     } else if (Math.random() <= (2/3)) {
-        return 'Paper'
-    } else {return 'Scissors'} 
-} // randomly returns either rock, paper or scissors
+        return 'paper'
+    } else {return 'scissors'} 
+}; // randomly returns either rock, paper or scissors
 
-let playerSelection = prompt("Do you want to choose Rock, Paper, or Scissors?")
+let computerSelection = getComputerChoice();
+let userInput = prompt('Do you want to choose rock, paper, or scissors?');
+let playerSelection = userInput.toLowerCase(); //to ensure case sameness
 
+function playGame (playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return 'You tied!';
+    } else if  (playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'paper') {
+        return `You win! ${playerSelection} beats ${computerSelection}!`; //win scenario
+    } else if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'rock') {
+        return `You lose! ${computerSelection} beats ${playerSelection}`; //lose scenario
+    };
+}
+
+console.log(playGame(playerSelection, computerSelection))
