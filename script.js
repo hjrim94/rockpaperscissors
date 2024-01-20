60eccc5ee3514cea08612fb8de28bd5b61082c30
hjrim94 <62858164+hjrim94@users.noreply.github.com>
2024-01-20 12:30:29 -0500

    Loop the function so the game runs 5 times and updates a counter for how many losses, wins and ties the player gets.

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
-function getComputerChoice () {
-    if (Math.random()<= (1/3)) {
-        return 'rock'
-    } else if (Math.random() <= (2/3)) {
-        return 'paper'
-    } else {return 'scissors'} 
-}; // randomly returns either rock, paper or scissors
+function game() {
+    let numOfWins = 0;
+    let numOfLosses = 0;
+    let numOfTies = 0;
 
-let computerSelection = getComputerChoice();
-let userInput = prompt('Do you want to choose rock, paper, or scissors?');
-let playerSelection = userInput.toLowerCase(); //to ensure case sameness
+    for (let i = 0; i < 5; i++) {
+        function getComputerChoice () {
+            if (Math.random()<= (1/3)) {
+                return 'rock'
+            } else if (Math.random() <= (2/3)) {
+                return 'paper'
+            } else {return 'scissors'} 
+        }; // randomly returns either rock, paper or scissors
 
-function playGame (playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return 'You tied!';
-    } else if  (playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'paper') {
-        return `You win! ${playerSelection} beats ${computerSelection}!`; //win scenario
-    } else if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'rock') {
-        return `You lose! ${computerSelection} beats ${playerSelection}`; //lose scenario
+        let computerSelection = getComputerChoice();
+        let userInput = prompt('Do you want to choose rock, paper, or scissors?');
+        let playerSelection = userInput.toLowerCase(); //to ensure case sameness
+
+        function playRound (playerSelection, computerSelection) {
+            if (playerSelection === computerSelection) {
+                console.log('You tied!');
+                numOfTies++;
+            } else if  (playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'paper') {
+                console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
+                numOfWins++; //win scenario
+            } else if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'rock') {
+                console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+                numOfLosses++; //lose scenario
+            } else {console.log('You need to input a correct choice!')};
+        }
+
+        playRound(playerSelection, computerSelection);
     };
+
 }
 
-console.log(playGame(playerSelection, computerSelection))
+game()
